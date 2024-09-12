41fe2ae815777abc8f1ae1862fef8aa113f074a5
banjoSolomon <ayomidebanjo68@gmail.com>
2024-09-13 00:16:43 +0100

    animate

--- src/Pages/Home/Section3/Section3.jsx
+++ src/Pages/Home/Section3/Section3.jsx
@@ -6,22 +6,22 @@ import logo11 from "../../../asset/face.png";
 import logo12 from "../../../asset/massage.png";
 
 const services = [
-    { name: 'Waxing', icon: <img src={logo10} alt="wax" className={styles.logo10}/> },
-    { name: 'Facials', icon: <img src={logo11} alt="face" className={styles.logo11}/> },
-    { name: 'Massage', icon: <img src={logo12} alt="massage" className={styles.logo12}/> },
+    { name: 'Waxing', icon: <img src={logo10} alt="wax" className={`${styles.logo10} wow animate__animated animate__backInLeft`} /> },
+    { name: 'Facials', icon: <img src={logo11} alt="face" className={`${styles.logo11} wow animate__animated animate__backInLeft`} /> },
+    { name: 'Massage', icon: <img src={logo12} alt="massage" className={`${styles.logo12} wow animate__animated animate__backInLeft`} /> },
 ];
 
 const Section3 = () => {
     return (
-        <div className={styles.sec}>
+        <div className={`${styles.sec} wow animate__animated animate__backInLeft`}>
             <div className={styles.our}>
-                <h2 className={styles.service}>Our Services</h2>
-                <p className={styles.description}>
+                <h2 className={`${styles.service} wow animate__animated animate__backInLeft`}>Our Services</h2>
+                <p className={`${styles.description} wow animate__animated animate__backInLeft`}>
                     A soothing environment in which you can rest, relax and feel completely rejuvenated.
                 </p>
                 <div className={styles.servicesContainer}>
                     {services.map((service) => (
-                        <div key={service.name} className={styles.serviceItem}>
+                        <div key={service.name} className={`${styles.serviceItem} wow animate__animated animate__backInLeft`}>
                             <div>{service.icon}</div>
                             <h3>{service.name}</h3>
                         </div>
--- src/Pages/Home/Section5/Section5.jsx
+++ src/Pages/Home/Section5/Section5.jsx
@@ -1,29 +1,28 @@
-import styles from './index.module.css'
-import logo13 from "../../../asset/pedicure.png";
+// Section5/index.js
 import React from "react";
-import logo20 from "../../../asset/tiz.png"
-import logo21 from "../../../asset/pro1.png"
+import styles from './index.module.css';
+import logo20 from "../../../asset/tiz.png";
+import logo21 from "../../../asset/pro1.png";
 
 const Section5 = () => {
-
     return (
-       <div className={styles.offers}>
-           <div className={styles.special}>
-               <p>Our Special Offers</p>
-               <p>20% Off All Spa Services: Enjoy a 20% discount on all wellness treatments.<br/>
-                   Treat yourself to the luxury you deserve.</p>
-               <p>
-                   Enroll in our specialized training sessions on holistic wellness, skincare,
-                   and massage techniques.<br/> Learn from the experts and take your skills to the next level.<br/>
-                   Hurry, these offers won't last forever!
-               </p>
-           </div>
-           <div>
-               <img src={logo20} alt="pedicure" className={styles.logo20}/>
-               <img src={logo21} alt="pedicure" className={styles.logo21}/>
-           </div>
-       </div>
-    )
+        <div className={`${styles.offers} wow animate__animated animate__zoomInDown`}>
+            <div className={`${styles.special} wow animate__animated animate__zoomInDown`}>
+                <p>Our Special Offers</p>
+                <p>20% Off All Spa Services: Enjoy a 20% discount on all wellness treatments.<br/>
+                    Treat yourself to the luxury you deserve.</p>
+                <p>
+                    Enroll in our specialized training sessions on holistic wellness, skincare,
+                    and massage techniques.<br/> Learn from the experts and take your skills to the next level.<br/>
+                    Hurry, these offers won't last forever!
+                </p>
+            </div>
+            <div className="wow animate__animated animate__zoomInDown">
+                <img src={logo20} alt="offer1" className={`${styles.logo20} wow animate__animated animate__zoomInDown`} />
+                <img src={logo21} alt="offer2" className={`${styles.logo21} wow animate__animated animate__zoomInDown`} />
+            </div>
+        </div>
+    );
 }
 
-export default Section5
+export default Section5;
