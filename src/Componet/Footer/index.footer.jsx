4556644628ac00d47c2cefc6eaa32b537d50ab4f
banjoSolomon <ayomidebanjo68@gmail.com>
2024-09-14 16:56:03 +0100

    form

--- src/Componet/Footer/index.footer.jsx
+++ src/Componet/Footer/index.footer.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+import { FaFacebook, FaTwitter, FaWhatsapp, FaInstagram } from 'react-icons/fa';
 import styles from './index.module.css';
 
 const Footer = () => {
@@ -6,7 +8,8 @@ const Footer = () => {
             <div className={styles.footerContainer}>
                 <div className={styles.footerSection}>
                     <h2>Recent Post</h2>
-                    <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since.</p>
+                    <p>“Embrace the new season with fresh beauty tips and wellness advice. Check out our latest post to
+                        discover how small changes can make a big difference in your daily routine.”</p>
                     <ul>
                         <li>About</li>
                         <li>FAQ</li>
@@ -18,25 +21,32 @@ const Footer = () => {
                 <div className={styles.footerSection}>
                     <h2>Working Hours</h2>
                     <ul>
-                        <li>Monday – Friday: 8am – 6pm EST</li>
-                        <li>Saturday: 9am – 5pm EST</li>
-                        <li>Sunday: 9am – 4pm EST</li>
+                        <li>Monday – Friday: 9am – 10pm</li>
+                        <li>Saturday: 10am – 10pm EST</li>
+                        <li>Sunday: 12am – 10pm EST</li>
                     </ul>
                 </div>
                 <div className={styles.footerSection}>
                     <h2>Contact Us</h2>
-                    <p>90 St Johns Brooklyn, NY, United States</p>
-                    <p>Email: contact.louisvillebeautysalon@gmail.com</p>
-                    <p>Phone: (+01) 888-999-777</p>
+                    <p>7 Adebare Street Ogudu</p>
+                    <p>Phone: (+234)8066306125</p>
                 </div>
             </div>
             <div className={styles.footerBottom}>
-                <p>© Copyright Louisville Beauty Salon. All rights reserved.</p>
+                <p>© Copyright Bee Heaven Beauty Spa. All rights reserved.</p>
                 <div className={styles.socialLinks}>
-                    <a href="#">Facebook</a>
-                    <a href="#">Twitter</a>
-                    <a href="#">Pinterest</a>
-                    <a href="#">Instagram</a>
+                    <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                        <FaFacebook />
+                    </a>
+                    <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+                        <FaTwitter />
+                    </a>
+                    <a href="https://wa.me/2348066306125" target="_blank" rel="noopener noreferrer">
+                        <FaWhatsapp />
+                    </a>
+                    <a href="https://www.instagram.com/beehavenbeautyspa/?hl=en" target="_blank" rel="noopener noreferrer">
+                        <FaInstagram />
+                    </a>
                 </div>
             </div>
         </div>
